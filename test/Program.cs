e58b69b8d45f6bf93ad1ce31dad72bcc75c87eea
Aksellercan <118260347+Aksellercan@users.noreply.github.com>
2022-12-27 22:46:38 +0100

    Update Program.cs

--- test/Program.cs
+++ test/Program.cs
@@ -1,21 +1,15 @@
-﻿using System;
-public class PrimesSieve
+using System;
+public class Primes
 {
     static void Main()
     {
         Console.WriteLine("Enter a value to count the primes ");
-         int a = int.Parse(Console.ReadLine());
-        // Create an array of boolean values indicating whether a number is prime.
-        // Start by assuming all numbers are prime by setting them to true.
+        int a = int.Parse(Console.ReadLine());
         bool[] primes = new bool[a + 1];
         for (int i = 0; i < primes.Length; i++)
         {
             primes[i] = true;
         }
-
-        // Loop through a portion of the array (up to the square root of MAX). If
-        // it's a prime, ensure all multiples of it are set to false, as they
-        // clearly cannot be prime.
         for (int i = 2; i < Math.Sqrt(a) + 1; i++)
         {
             if (primes[i - 1])
@@ -26,8 +20,6 @@ public class PrimesSieve
                 }
             }
         }
-
-        // Output the results
         int count = 0;
         for (int i = 2; i < primes.Length; i++)
         {
